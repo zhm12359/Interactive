805c116e7fab0dccba4929ce713ba894c66d5cc8
zhm12359 <hz926@nyu.edu>
2017-09-09 21:35:39 -0400

    git ignore

--- assignment01/sketch.js
+++ assignment01/sketch.js
@@ -1,22 +1,71 @@
 function setup() {
     // set the background size of our canvas
-    createCanvas(400, 400);
+    createCanvas(800, 800);
 
     // erase the background with a "grey" color
-    background(100);
+    background("lightblue");
 
-    // set all content drawn from this point forward
-    // so that it uses "white" (0 = black, 255 = white)
-    fill(255);
+    noStroke();
 
-    // write some text at position 100,100
-    text("Hello, World!", 100,100);
+    //house body
+    fill(133,133,133, 150);
+    rect(200,400, 400,400);
 
-    // draw a rectangle at position 100,200
-    // size = 50x50
-    rect(100,200,50,50);
+    //rooftop
+    fill("brown");
+    triangle(150, 400, 650, 400, 400, 200);
 
-    // draw an ellipse at 200,200
-    // radius = 25
-    ellipse(200,200, 25, 25);
+    //door
+    fill("black");
+    rect(450, 600, 100,200);
+
+    //door handle
+    fill("yellow");
+    ellipse(530, 700, 20, 20);
+
+    //window
+    drawWindow(250,500,45);
+
+    //sun
+    stroke("yellow");
+    fill("red");
+    ellipse(0,0, 150, 150);
+    noStroke();
+
+    //clouds
+    drawCloud(150,100);
+    drawCloud(200,150);
+    drawCloud(350,170);
+    drawCloud(620,170);
+    drawCloud(550,100);
+    drawCloud(400,90);
+
+
+    fill("black");
+    textSize(36);
+    text("My Lovely House", 250, 70);
+
 }
+
+
+function drawCloud(x, y){
+    fill("while");
+    ellipse(x,y, 50,20);
+    ellipse(x+40,y, 50,20);
+    ellipse(x+20,y-10, 50,20);
+}
+
+function drawWindow(x,y,size){
+    stroke("black");
+    fill("lightgreen");
+    rect(x,y,size,size);
+    rect(x+size,y,size,size);
+    rect(x,y+size,size,size);
+    rect(x+size,y+size,size,size);
+
+
+
+    noStroke();
+
+}
+
