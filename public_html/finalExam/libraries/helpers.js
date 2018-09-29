d2e0ec966e1f79528a89e2cf9e491e8fe48cce5e
zhm12359 <hz926@nyu.edu>
2018-09-29 13:46:39 -0400

    re-organize

--- public_html/finalExam/libraries/helpers.js
+++
@@ -1,9 +0,0 @@
-// helper functions	
-Number.prototype.map = function (in_min, in_max, out_min, out_max) {
-	return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}			
-
-// Converts from degrees to radians.
-Math.radians = function(degrees) {
-	return degrees * Math.PI / 180;
-};
